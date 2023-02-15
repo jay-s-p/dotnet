7aeb6449cbb68871bfaec0081b2fa0a5735c04d2
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-02-15 09:20:46 +0530

    Added prog 4 & 5 in exp-3 -

---
+++ EXP-3/p4.cs
@@ -0,0 +1,37 @@
+/*
+Design form to select image name from list and display in picture box. 
+*/
+namespace Zzz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex == 0)
+            {
+                pictureBox.ImageLocation = "z://pic1.jpg";
+            }
+            else if (listBox.SelectedIndex == 1)
+            {
+                pictureBox.ImageLocation = "z://pic2.jpg";
+            }
+            else if (listBox.SelectedIndex == 2)
+            {
+                pictureBox.ImageLocation = "z://pic3.jpg";
+            }
+            else if (listBox.SelectedIndex == 3)
+            {
+                pictureBox.ImageLocation = "z://pic4.jpg";
+            }
+            else if (listBox.SelectedIndex == 4)
+            {
+                pictureBox.ImageLocation = "z://pic5.jpg";
+            }
+        }
+    }
+}
---
+++ EXP-3/p5.cs
@@ -0,0 +1,72 @@
+/*
+Design a form to use MenuStrip which having File,Window and Style using MDI Form.
+*/
+
+namespace Zzz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new Form2();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void form3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new Form3();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void closeAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form fr in MdiChildren)
+                fr.Close();
+        }
+
+        private void noOfChildsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No. of childred is " + MdiChildren.Length);
+        }
+
+        private void maximizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach(Form fr in MdiChildren)
+                fr.WindowState = FormWindowState.Maximized;
+        }
+
+        private void normalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form fr in MdiChildren)
+                fr.WindowState = FormWindowState.Normal;
+        }
+        private void minimizeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            foreach (Form fr in MdiChildren)
+                fr.WindowState = FormWindowState.Minimized;
+        }
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+    }
+}
